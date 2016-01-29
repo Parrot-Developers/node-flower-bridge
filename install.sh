551aabfb4e08c7c38ef7c99fd5a7096f8d6a9a38
bsautron <sautron.brunojj@gmail.com>
2016-01-29 12:32:27 +0100

    Dont need sudo password for nodejs

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo setcap cap_net_raw+eip $(eval readlink -f `which node`)
 (git clone -b v2 https://github.com/Parrot-Developers/node-flower-power && cd node-flower-power && npm install)
 (git clone -b v2 https://github.com/Parrot-Developers/node-flower-power-cloud && cd node-flower-power-cloud && npm install)
 npm install
